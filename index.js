5971f56e62ef880806f59cb8388271843cbc3007
Zac Allen <allenzac321@gmail.com>
2022-10-22 11:27:18 -0400

    budget code

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+
+}
+for(let m = 0; m < monthlyExpenseQuestions.length; m++){
+    let manswer = parseFloat(window.prompt(monthlyExpenseQuestions[m]))
+    monthlyExpenses = monthlyExpenses + manswer
+}
+for(let a = 0; a < annualExpenseQuestions.length; a++){
+    let answer = parseFloat(window.prompt(annualExpenseQuestions[a]))
+    annualExpenses = annualExpenses + answer
+}
